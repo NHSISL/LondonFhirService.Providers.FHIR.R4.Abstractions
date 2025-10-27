4a1a052fe645f6b397d5a7108e6a610b7511b5ef
Christo du Toit <christo@dutoit.co.uk>
2025-10-27 15:09:41 +0000

    CODE RUB: Code cleanup

--- LondonFhirService.Providers.FHIR.R4.Abstractions/Operations/IResourceOperation.cs
+++ LondonFhirService.Providers.FHIR.R4.Abstractions/Operations/IResourceOperation.cs
@@ -1,3 +1,7 @@
+// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
 using System;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Rest;
