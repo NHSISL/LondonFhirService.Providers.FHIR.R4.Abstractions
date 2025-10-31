16b0ccfb4b9140ed9f25ff69313efe0878cf0e9c
Christo du Toit <christo@dutoit.co.uk>
2025-10-31 10:43:02 +0000

    CODE RUB: Code cleanup

--- LondonFhirService.Providers.FHIR.R4.Abstractions.Tests.Unit/Models/EmptyFhirProvider.cs
+++ LondonFhirService.Providers.FHIR.R4.Abstractions.Tests.Unit/Models/EmptyFhirProvider.cs
@@ -5,5 +5,11 @@
 namespace LondonFhirService.Providers.FHIR.R4.Abstractions.Tests.Unit.Models
 {
     public sealed class EmptyFhirProvider : FhirProviderBase, IFhirProvider
-    { }
+    {
+        public override string Source => throw new System.NotImplementedException();
+
+        public override string Code => throw new System.NotImplementedException();
+
+        public override string System => throw new System.NotImplementedException();
+    }
 }
--- LondonFhirService.Providers.FHIR.R4.Abstractions.Tests.Unit/Models/TestFhirProvider.cs
+++ LondonFhirService.Providers.FHIR.R4.Abstractions.Tests.Unit/Models/TestFhirProvider.cs
@@ -9,5 +9,11 @@ namespace LondonFhirService.Providers.FHIR.R4.Abstractions.Tests.Unit.Models
     public sealed class TestFhirProvider : FhirProviderBase, IFhirProvider
     {
         public override IPatientResource Patients => new TestPatientResource();
+
+        public override string Source => throw new System.NotImplementedException();
+
+        public override string Code => throw new System.NotImplementedException();
+
+        public override string System => throw new System.NotImplementedException();
     }
 }
